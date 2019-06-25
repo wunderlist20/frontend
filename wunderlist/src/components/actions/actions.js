4f637c3c1990558b3835fd9bc5755ad95c088957
Katie Rose <katiefelten@gmail.com>
2019-06-25 18:38:17 -0400

    removed server.js, WIP: fetching api from BE

--- wunderlist/src/components/actions/actions.js
+++ wunderlist/src/components/actions/actions.js
@@ -15,7 +15,7 @@ export const login = creds => dispatch => {
   dispatch({ type: LOGIN_START });
 
   return axios
-    .post("http://localhost:5000/api/login", creds)
+    .post("https://wunderlist-02.herokuapp.com/api/auth/login", creds)
     .then(res => {
       localStorage.setItem("token", res.data.payload);
       dispatch({ type: LOGIN_SUCCESS });
