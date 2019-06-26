96a2930d63eae7cfecc8551ccbaabdc02c2f496c
Katie Rose <katiefelten@gmail.com>
2019-06-26 10:56:41 -0400

    actions updated, need to upload to netlify

--- wunderlist/src/components/actions/actions.js
+++ wunderlist/src/components/actions/actions.js
@@ -4,12 +4,16 @@ export const LOGIN_START = "LOGIN_START";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
 
+export const DELETE_TODO = "DELETE_TODO";
+
 export const FETCH_TODOS_START = "FETCH_TODOS_START";
 export const FETCH_TODOS_SUCCESS = "FETCH_TODOS_SUCCESS";
 export const FETCH_TODOS_FAILURE = "FETCH_TODOS_FAILURE";
 
 export const ADD_TODO = "ADD_TODO";
 export const ADD_TODO_FAILURE = "ADD_TODO_FAILURE";
+
+// Login
 
 export const login = creds => dispatch => {
   dispatch({ type: LOGIN_START });
@@ -25,22 +29,14 @@ export const login = creds => dispatch => {
     });
 };
 
-// export const delete = id => dispatch => {
+// Register new user
 
-//   return axios
-// .delete('http://localhost:5000/api/${id}')
-// .then(res => {
-//   dispatch({ type: DELETE, payload: id});
-// })
-// .catch(err => {
-//   console.log(err)
-// });
-// };
+// Fetch Todos
 
 export const fetchTodos = () => dispatch => {
   dispatch({ type: FETCH_TODOS_START });
   axios
-    .get("http://localhost:5000/api/todos", {
+    .get("https://wunderlist-02.herokuapp.com/api/todos/", {
       headers: { Authorization: localStorage.getItem("token") }
     })
     .then(res => {
@@ -51,9 +47,11 @@ export const fetchTodos = () => dispatch => {
     });
 };
 
+// Add Todo - add a new todo, and return the created object. title, task and setDate are required fields. user_id, notes, and completed are optional fields.
+
 export const addTodo = newTodo => dispatch => {
   axios
-    .post("http://localhost:5000/api/todos", newTodo, {
+    .post("https://wunderlist-02.herokuapp.com/api/todos/", newTodo, {
       headers: { Authorization: localStorage.getItem("token") }
     })
     .then(res => {
@@ -65,3 +63,25 @@ export const addTodo = newTodo => dispatch => {
       dispatch({ type: ADD_TODO_FAILURE, payload: err.response });
     });
 };
+
+// Delete Todo
+
+export const deleteTodo = id => dispatch => {
+
+  return axios
+.delete('https://wunderlist-02.herokuapp.com/api/todos/:id')
+.then(res => {
+  dispatch({ type: DELETE_TODO, payload: id});
+})
+.catch(err => {
+  console.log(err)
+});
+};
+
+// Edit Todo
+
+// View profile by ID
+
+// Edit profile
+
+// Delete profile
