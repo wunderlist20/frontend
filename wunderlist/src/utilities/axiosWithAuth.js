18e45bb26f1828e40e1627da467bd30a0f9b40fc
Jonathan Picazo <macbethjonathan@gmail.com>
2019-06-27 12:22:15 -0400

    set baseURL in axios options

--- wunderlist/src/utilities/axiosWithAuth.js
+++ wunderlist/src/utilities/axiosWithAuth.js
@@ -4,6 +4,7 @@ export const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
 
   return axios.create({
+    baseURL: 'https://wunderlist-02.herokuapp.com/api',
     headers: {
       Authorization: token
     }
